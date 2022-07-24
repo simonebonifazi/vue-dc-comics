7ecf19dba887af155b69e19a23d4ea7bc54a27f0
simonebonifazi <simonbonifraz@gmail.com>
2022-07-24 12:45:21 +0200

    fixed-layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <MyLayout />
+    <MyHeader />
   </div>
 </template>
 
 <script>
-import MyLayout from './components/MyLayout.vue'
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'dc-comics',
   components: {
-    MyLayout
+    MyHeader
   }
 }
 </script>
 
 <style lang="scss">
+//layout
+div {
+  border: 2px dashed blue;
+  min-height: 80px;
+}
+
+section {
+  border: 2px solid green;
+  min-height: 250px;
+}
+
 //Reset
 * {
-  font-family: "Segoe UI", "Open Sans", Helvetica, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 //Generics
 #app {
   font-family: "Segoe UI", "Open Sans", Helvetica, sans-serif;
+}
+
+img {
+  height: auto;
+  max-width: 100%;
+}
+
+ul {
+  list-style-type: none;
 }
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container">
-
+    ciao a tutti
   </div>
 
 </template>
 
 <script>
 export default {
-  name: 'MyLayout'
+  name: 'MyHeader'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style>
 </style>
